ec3a65c8391dfa206b474ffe3227744832552e6a
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-26 14:53:36 +0300

    add bow algorithm

---
+++ models/bowNLP.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv("../data/cell_phones.tsv", sep='\t')
+df.head()
+
+df.dropna(inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+# Cleaning the texts
+
+nltk.download('stopwords')
+
+corpus = []
+
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', df['review text'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if word not in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = df.iloc[:, 0].values[:1000]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
